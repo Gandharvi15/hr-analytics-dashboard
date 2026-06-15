69f42b237257ba4f9f7dd00e000bd2c401ccef55
Gandharvi Patil <gandharvip@gmail.com>
2026-06-16 00:25:09 +0530

    Delete sql/Q2.sql

--- sql/Q2.sql
+++
@@ -1,9 +0,0 @@
--- Average salary by department AND attrition status
-select Department , Attrition, 
-count(*) as total_emp, 
-round(avg(MonthlyIncome),0) as avg_monthly_income,
-min(MonthlyIncome),
-max(MonthlyIncome)
-from employee
-group by Department , Attrition 
-order by Department, Attrition desc;
